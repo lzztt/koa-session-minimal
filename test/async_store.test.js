621f74fefd7269d7702b3ca2794998eb5d35c44f
Longzhang Tian <15257546+longztian@users.noreply.github.com>
2016-09-06 22:35:45 -0500

    add support for generator store

---
+++ test/async_store.test.js
@@ -0,0 +1,162 @@
+const Koa = require('koa')
+const request = require('supertest')
+const expect = require('chai').expect
+const session = require('../lib/session')
+
+class AsyncSpyStore {
+  constructor() {
+    this.sessions = {}
+    this.calls = {
+      get: [],
+      set: [],
+      destroy: [],
+    }
+  }
+
+  async get(sid) {
+    this.calls.get.push([...arguments])
+    return this.sessions[sid]
+  }
+
+  async set(sid, val = {}) {
+    this.calls.set.push([...arguments])
+    this.sessions[sid] = val
+  }
+
+  async destroy(sid) {
+    this.calls.destroy.push([...arguments])
+    delete this.sessions[sid]
+  }
+}
+
+const sessionBody = ctx => {
+  if (Object.keys(ctx.session).length === 0) {
+    ctx.session.id = ctx.sessionHandler.getId()
+    ctx.session.time = Date.now()
+  }
+  ctx.body = ctx.session
+}
+
+const validateCookie = (res, key) => {
+  const cookie = res.header['set-cookie']
+  expect(cookie.length).to.be.equal(1)
+  expect(cookie[0].slice(0, key.length + 35)).to.be.equal(`${key}=${res.body.id}; `)
+}
+const validateBody = (res, startTime) => {
+  expect(res.body.time).to.be.at.least(startTime)
+  expect(res.body.time).to.be.at.most(Date.now())
+}
+
+describe('session with async store', () => {
+  const app = new Koa()
+  const key = 'koa:sess'
+  const store = new AsyncSpyStore()
+
+  app.use(session({
+    store
+  }))
+  app.use(sessionBody)
+
+  const client = request(app.listen())
+
+  it('should work and set session cookie', (done) => {
+    const startTime = Date.now()
+    client.get('/')
+      .expect(200)
+      .end((err, res) => {
+        if (err) done(err)
+        validateCookie(res, key)
+        validateBody(res, startTime)
+        done()
+      })
+  })
+
+  it('should work when multiple clients access', done => {
+    const startTime = Date.now()
+    Promise.all([
+      new Promise((resolve, reject) => {
+        client.get('/').end((err, res) => {
+          if (err) reject(err)
+          validateCookie(res, key)
+          validateBody(res, startTime)
+          resolve(res.body.id)
+        })
+      }),
+      new Promise((resolve, reject) => {
+        client.get('/').end((err, res) => {
+          if (err) reject(err)
+          validateCookie(res, key)
+          validateBody(res, startTime)
+          resolve(res.body.id)
+        })
+      })
+    ]).then(sids => {
+      expect(sids[0]).to.be.not.equal(sids[1])
+      done()
+    }).catch((err) => {
+      done(err)
+    })
+  })
+
+  it('session data is available among multiple requests', done => {
+    const startTime = Date.now()
+    client.get('/').end((err1, res1) => {
+      if (err1) done(err1)
+      validateCookie(res1, key)
+      validateBody(res1, startTime)
+      const session = res1.body
+
+      client.get('/').end((err2, res2) => {
+        if (err2) done(err2)
+        validateCookie(res2, key)
+        validateBody(res2, startTime)
+        expect(res2.body.id).to.be.not.equal(session.id)
+        expect(res2.body.time).to.be.at.least(session.time)
+
+        client.get('/').set('cookie', `${key}=${session.id}`).end((err3, res3) => {
+          if (err3) done(err3)
+          expect(res3.header['set-cookie']).to.be.equal(undefined)
+          expect(res3.body).to.be.deep.equal(session)
+          done()
+        })
+      })
+    })
+  })
+
+  it('store method call times', done => {
+    const app = new Koa()
+    const key = 'koa:sess'
+    const store = new AsyncSpyStore()
+
+    app.use(session({
+      store
+    }))
+    app.use(sessionBody)
+
+    const client = request(app.listen())
+
+    client.get('/').end((err1, res1) => {
+      if (err1) done(err1)
+      const session = res1.body
+      expect(store.calls.get.length).to.be.equal(0)
+      expect(store.calls.set.length).to.be.equal(1)
+      expect(store.calls.destroy.length).to.be.equal(0)
+
+
+      client.get('/').end((err2, res2) => {
+        if (err2) done(err2)
+        expect(store.calls.get.length).to.be.equal(0)
+        expect(store.calls.set.length).to.be.equal(2)
+        expect(store.calls.destroy.length).to.be.equal(0)
+
+        client.get('/').set('cookie', `${key}=${session.id}`).end((err3, res3) => {
+          if (err3) done(err3)
+          expect(store.calls.get.length).to.be.equal(1)
+          expect(store.calls.set.length).to.be.equal(2)
+          expect(store.calls.destroy.length).to.be.equal(0)
+          done()
+        })
+      })
+    })
+  })
+})
