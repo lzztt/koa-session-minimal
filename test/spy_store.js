736bbe855db456447d3966c7aa6a82d968d237a4
Longzhang Tian <15257546+longztian@users.noreply.github.com>
2016-09-08 23:47:03 -0500

    add more tests in default_store test

---
+++ test/spy_store.js
@@ -0,0 +1,64 @@
+class AsyncSpyStore {
+  constructor() {
+    this.clear()
+  }
+
+  clear() {
+    this.sessions = {}
+    this.calls = {
+      get: [],
+      set: [],
+      destroy: [],
+    }
+  }
+
+  async get(sid) {
+    this.calls.get.push([...arguments])
+    return this.sessions[sid]
+  }
+
+  async set(sid, val, ttl) { // eslint-disable-line no-unused-vars
+    this.calls.set.push([...arguments])
+    this.sessions[sid] = val
+  }
+
+  async destroy(sid) {
+    this.calls.destroy.push([...arguments])
+    delete this.sessions[sid]
+  }
+}
+
+class GeneratorSpyStore {
+  constructor() {
+    this.clear()
+  }
+
+  clear() {
+    this.sessions = {}
+    this.calls = {
+      get: [],
+      set: [],
+      destroy: [],
+    }
+  }
+
+  * get(sid) {
+    this.calls.get.push([...arguments])
+    return this.sessions[sid]
+  }
+
+  * set(sid, val, ttl) { // eslint-disable-line no-unused-vars
+    this.calls.set.push([...arguments])
+    this.sessions[sid] = val
+  }
+
+  * destroy(sid) {
+    this.calls.destroy.push([...arguments])
+    delete this.sessions[sid]
+  }
+}
+
+module.exports = {
+  AsyncSpyStore,
+  GeneratorSpyStore,
+}
