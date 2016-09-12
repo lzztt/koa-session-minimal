91cff65ae4ee212c54b4decc68aaef68c0472c39
Long <15257546+lzztt@users.noreply.github.com>
2016-09-11 22:44:43 -0500

    v1.1.0 remove public interface sessionHandler.getId()

--- example/counter.js
+++ example/counter.js
@@ -12,12 +12,10 @@ app.use(session({
 app.use(async (ctx, next) => {
   ctx.session.count = ctx.session.count || 0
   if (ctx.path === '/add') ctx.session.count++
-  await next()
-})
 
-// populate response body
-app.use(ctx => {
-  ctx.body = ctx.sessionHandler.getId() + ' : ' + ctx.session.count
+  await next()
+
+  ctx.body = ctx.session.count
 })
 
 app.listen(3000)
