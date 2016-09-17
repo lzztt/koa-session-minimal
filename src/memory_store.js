cbfe8d7b4965608cc2ce9a44c7880339dd0d01de
Longzhang Tian <15257546+longztian@users.noreply.github.com>
2016-09-17 14:36:30 -0500

    add ttl support for default memory store

--- src/memory_store.js
+++ src/memory_store.js
@@ -1,17 +1,22 @@
 module.exports = class MemoryStore {
   constructor() {
     this.sessions = {}
+    this.timeouts = {}
   }
 
   get(sid) {
     return this.sessions[sid]
   }
 
-  set(sid, val, ttl) { // eslint-disable-line no-unused-vars
+  set(sid, val, ttl) {
     this.sessions[sid] = val
+
+    if (sid in this.timeouts) clearTimeout(this.timeouts[sid])
+    this.timeouts[sid] = setTimeout(this.destroy.bind(this, sid), ttl)
   }
 
   destroy(sid) {
     delete this.sessions[sid]
+    delete this.timeouts[sid]
   }
 }
