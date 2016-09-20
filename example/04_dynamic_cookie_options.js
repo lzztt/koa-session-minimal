eff68862bc941a41bf214739e10db5b0d4470bdf
Longzhang Tian <15257546+longztian@users.noreply.github.com>
2016-09-19 21:17:31 -0500

    add examples

---
+++ example/04_dynamic_cookie_options.js
@@ -0,0 +1,30 @@
+const Koa = require('koa')
+const session = require('..')
+const RedisStore = require('koa-redis')
+
+const app = new Koa()
+
+const ONE_MONTH = 30 * 24 * 3600 * 1000
+
+app.use(session({
+  store: new RedisStore(),
+  cookie: (ctx) => {
+    return {
+      maxAge: ctx.session.user ? ONE_MONTH : 0,
+      httpOnly: false,
+    }
+  },
+}))
+
+const counter = (ctx) => {
+  if (ctx.path === '/favicon.ico') return // ignore favicon
+
+  if (!('count' in ctx.session)) ctx.session.count = 0 // init counter
+
+  ctx.session.count += 1
+  ctx.body = ctx.session.count
+}
+
+app.use(counter)
+
+app.listen(3000)
