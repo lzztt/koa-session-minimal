92c614b4243da2522cadc2d19d7f5d5788e19484
longztian <ikki3355@gmail.com>
2016-09-19 21:17:31 -0500

    add examples

---
+++ example/02_redis_store.js
@@ -0,0 +1,22 @@
+const Koa = require('koa')
+const session = require('..')
+const RedisStore = require('koa-redis')
+
+const app = new Koa()
+
+app.use(session({
+  store: new RedisStore(),
+}))
+
+const counter = (ctx) => {
+  if (ctx.path === '/favicon.ico') return // ignore favicon
+
+  if (!('count' in ctx.session)) ctx.session.count = 0 // init counter
+
+  ctx.session.count += 1
+  ctx.body = ctx.session.count
+}
+
+app.use(counter)
+
+app.listen(3000)
